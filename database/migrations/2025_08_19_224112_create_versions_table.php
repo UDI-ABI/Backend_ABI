3524e94fce725deb92dddef44c1786b102bd9be3
AngieHdez <137322592+AngieHdez@users.noreply.github.com>
2025-08-20 20:12:47 -0500

    Update 2025_08_19_224112_create_versions_table.php
    Eliminación del campo date en versions

--- database/migrations/2025_08_19_224112_create_versions_table.php
+++ database/migrations/2025_08_19_224112_create_versions_table.php
@@ -13,7 +13,6 @@ return new class extends Migration
     {
         Schema::create('versions', function (Blueprint $table) {
             $table->id();
-            $table->date('date');
             $table->foreignId('project_id')
                 ->constrained('projects')
                 ->onDelete('cascade')
