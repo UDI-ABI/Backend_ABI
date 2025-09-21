20d7f02ccda6ab895d773f9010f79bf38916bbaa
AngieHdez <ahernandez46@udi.edu.co>
2025-09-20 22:16:46 -0500

    Orden de los seeders en database

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -2,7 +2,22 @@
 
 namespace Database\Seeders;
 
+use App\Models\City;
+use App\Models\CityProgram;
+use App\Models\Content;
+use App\Models\ContentFramework;
+use App\Models\ContentFrameworkProject;
+use App\Models\InvestigationLine;
+use App\Models\Professor;
+use App\Models\Program;
+use App\Models\ProjectStatus;
+use App\Models\ResearchGroup;
+use App\Models\ResearchStaff;
+use App\Models\Student;
+use App\Models\ThematicArea;
 use App\Models\User;
+use App\Models\Version;
+use Google\Service\AIPlatformNotebooks\Resource\Projects;
 // use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
 
@@ -13,6 +28,28 @@ class DatabaseSeeder extends Seeder
      */
     public function run(): void
     {
-        $this->call([FrameworksTableSeeder::class]);
+        $this->call([
+            UsersTableSeeder::class,
+            ResearchStaffTableSeeder::class,
+            DepartmentsTableSeeder::class,
+            CitiesTableSeeder::class,
+            ResearchGroupsTableSeeder::class,
+            ProgramsTableSeeder::class,
+            CityProgramTableSeeder::class,
+            ProfessorsTableSeeder::class,
+            StudentsTableSeeder::class,
+            InvestigationLinesTableSeeder::class,
+            ThematicAreasTableSeeder::class,
+            ProjectStatusesTableSeeder::class,
+            ProjectsTableSeeder::class,
+            ProfessorProjectTableSeeder::class,
+            StudentProjectTableSeeder::class,
+            FrameworksTableSeeder::class,
+            ContentFrameworksTableSeeder::class,
+            ContentFrameworkProjectTableSeeder::class,
+            VersionsTableSeeder::class,
+            ContentsTableSeeder::class,
+            ContentVersionTableSeeder::class,
+        ]);
     }
 }
