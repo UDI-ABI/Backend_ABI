01ec7538b54cda08c2c6f4eb6615f2993c657296
Miguel Becerra <125840672+MiguelBecerra219@users.noreply.github.com>
2025-09-27 17:36:19 -0500

    Comentarios en las migraciones

--- database/seeders/FrameworksTableSeeder.php
+++ database/seeders/FrameworksTableSeeder.php
@@ -3,7 +3,20 @@
 namespace Database\Seeders;
 use JeroenZwart\CsvSeeder\CsvSeeder;
 
-
+/**
+ * Seeder to load initial data into the database from a CSV file.
+ *
+ * This seeder uses the laravel-csv-seeder package by jeroenzwart to efficiently import data.
+ * It follows the naming convention where the CSV file has the same name as the target table.
+ *
+ * Key features:
+ * - Does not truncate the table before inserting (allows multiple executions)
+ * - Uses comma (,) as the default delimiter
+ * - The CSV file must be located in /database/seeders/csvs/
+ * - There is no need to specify the table name, as it is inferred from the CSV filename
+ *
+ * Note: If you need to completely reset the data, set $this->truncate to true
+ */
 class FrameworksTableSeeder extends CsvSeeder
 {
 
@@ -11,10 +24,13 @@ class FrameworksTableSeeder extends CsvSeeder
     {
         $this->file = '/database/seeders/csvs/frameworks.csv';
         $this->delimiter = ',';
-        $this->truncate = false; // ⬅️ Evita el TRUNCATE
+        $this->truncate = false; // ⬅️ Avoid TRUNCATE
     }
     /**
-     * Run the database seeds.
+     * Runs the seeder to insert data into the database.
+     *
+     * This method calls the parent class from laravel-csv-seeder, which handles the import
+     * process from the CSV file specified in the constructor.
      */
     public function run(): void
     {
